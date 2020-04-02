e1974490a2282e85d615a6b5550175838be2d83f
adavis-85 <58529391+adavis-85@users.noreply.github.com>
2020-04-02 15:40:01 -0700

    Update Knapsack_and_tsp_test.jl

--- Knapsack_and_tsp_test.jl
+++ Knapsack_and_tsp_test.jl
@@ -31,45 +31,57 @@ while length(weights)>0
             
     
 cap=100
-    
+
+##Sets the incumbent for each following function.  
 incumbents=zeros(length(weights)+3)
 
 x_test=Tuple{Int,Int}[]
 
+##Solving the first LP relaxation i.e. branch and bound.
 d,mark=initial_lp(weights,cap,incumbents)
 
 if d!=0
+##The greater of the two paths from the selected node.  Each smaller is ignored (pruned) and the greater is then split
+##again.
 e,f,incumbents=split_func(d,x_test,mark)
 
 while  f!=0
+##Splits again on a loop until found infeasible.
 e,f,incumbents=split_func(f,e,incumbents)
 end
 else
     incumbents=mark
 end
-
-##here is where its not working.
-            
+     
+##The array for the items picked from the branch and bound.
 test_var=[]
-need=[]               
+##The array for the items not picked to be able to iterate over the next time through the loop.
+need=[]        
+    
 for i::Int in 1:length(weights)
-if incumbents[i]==1
+    if incumbents[i]==1
         push!(test_var,i)
-                    else
-                        push!(need,i)
-                end
-                    end       
-         
+    else
+        push!(need,i)
+    end
+end       
+     
+##Variable to iterate over to show the trip after the traveling salesman is solved.
 d=weights[test_var]
-                
+
+##Points selected from the optimized knapsack problem.
 p=points_for_distance[test_var]
 
+##Setting the points for the next time through the loop.  Selected points will not be selected again.
 points_for_distance=points_for_distance[need]
-                
+
+##Function to construct the distance matrix.
 Distances=distance_matrix(p)
 
+##Function to solve the distance matrix for the minimum path.
 solution,z_value=b_and_b_tsp(Distances)
-            
+ 
+##The trips selected from the maximum carried and the minimum distance.
 for i in 1:length(solution)
     if solution[i]!=1
     push!(pin,((p[solution[i]-1],d[solution[i]-1]),i-1))
@@ -78,6 +90,7 @@ for i in 1:length(solution)
 end
 end
 
+##Weights selected will not be selected again.
     weights=weights[need]
                 
 end
